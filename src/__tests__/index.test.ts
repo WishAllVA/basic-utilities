0945f1bd7a0c52d5e1824b8f61f1a5197d193c0c
WishAllVA <aroravishal629@gmail.com>
2021-02-21 15:56:04 +0530

    Adding palindrome, invert object, flatten object and get excel column index functions

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,4 +1,12 @@
-import { filterObject, getSpecificKeys, compareArrays } from '../index';
+import {
+  filterObject,
+  getSpecificKeys,
+  compareArrays,
+  isPalindrome,
+  flattenObject,
+  getIndexOfExcelColumn,
+  invertObject,
+} from '../index';
 test('Filter Object', () => {
   const unfilteredObject: object = { a: 'Hello', b: 'Bye' };
   const value: string = 'Bye';
@@ -18,4 +26,26 @@ test('Compare Arrays', () => {
   expect(compareArrays([1, 2, 3], [1, 2, 3], 1)).toBe(true);
   expect(compareArrays([1, 2, 3], [2, 1, 3], 0)).toBe(true);
   expect(compareArrays([1, 2, 3], [2, 1, 3], 1)).toBe(false);
+});
+
+test('Check palindrome', () => {
+  expect(isPalindrome('Hello')).toBe(false);
+  expect(isPalindrome('AWSWSDDSWSWA')).toBe(true);
+});
+
+test('Flatten Object', () => {
+  const testObject: object = { a: 'Hello', b: { c: 'Bye' } };
+  const flattenedObj = flattenObject(testObject);
+  expect(JSON.stringify(flattenedObj)).toBe(JSON.stringify({ a: 'Hello', c: 'Bye' }));
+});
+
+test('Get column index', () => {
+  expect(getIndexOfExcelColumn('AZ')).toBe(52);
+  expect(getIndexOfExcelColumn('AAA')).toBe(703);
+});
+
+test('Invert Object', () => {
+  const testObject: object = { a: 'Hello', c: 'Bye' };
+  const invertedObj = { Hello: 'a', Bye: 'c' };
+  expect(JSON.stringify(invertObject(testObject))).toBe(JSON.stringify(invertedObj));
 });
--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,22 @@
  * @param obj Object whose keys has to be get
  * @param value Condition for the keys
  */
-export function getSpecificKeys(obj: object, value: string) {
+export const getSpecificKeys = (obj: object, value: string) => {
   return Object.keys(obj).filter((key: string) => obj[key] === value);
-}
+};
 
 /**
  *
  * @param obj Object whose keys has to be filtered
  * @param value Value of the keys that has to be filtered
  */
-export function filterObject(obj: object, value: string) {
+export const filterObject = (obj: object, value: string) => {
   const filteredObj = {};
   Object.keys(obj).forEach((key: string) =>
     obj[key] === value ? Object.assign(filteredObj, { [key]: obj[key] }) : null,
   );
   return filteredObj;
-}
+};
 
 /**
  *
@@ -26,7 +26,42 @@ export function filterObject(obj: object, value: string) {
  * @param arrB Second Array
  * @param order Check Order ? 0 for no, 1 for yes
  */
-export function compareArrays(arrA: number[], arrB: number[], order: number) {
+export const compareArrays = (arrA: number[], arrB: number[], order: number): Boolean => {
   if (+order) return JSON.stringify(arrA) === JSON.stringify(arrB);
   return JSON.stringify(arrA.sort()) === JSON.stringify(arrB.sort());
-}
+};
+
+/**
+ *
+ * @param str String which has to be checked if it is palindrome
+ */
+export const isPalindrome = (str: string) => str === str.split('').reverse().join('');
+
+/**
+ *
+ * @param obj Object that has to be flattened
+ */
+export const flattenObject = (obj: object) => {
+  return Object.assign(
+    {},
+    ...(function _flatten(o: object): any {
+      return [].concat(
+        ...Object.keys(o).map((k: string) => (typeof o[k] === 'object' ? _flatten(o[k]) : { [k]: o[k] })),
+      );
+    })(obj),
+  );
+};
+
+/**
+ *
+ * @param col Column name of excel file
+ */
+export const getIndexOfExcelColumn = (col: string) =>
+  col.split('').reduce((prev, next) => prev * 26 + parseInt(next, 36) - 9, 0);
+
+/**
+ *
+ * @param obj Object whose keys and values has to be inverted
+ */
+export const invertObject = (obj: object) =>
+  Object.keys(obj).reduce((res, k) => Object.assign(res, { [obj[k]]: k }), {});
